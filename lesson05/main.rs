f26e274466e34eeb0d3efe45adcb502e95ac3c09
Ysgard <ysgard@gmail.com>
2016-11-19 23:30:59 -0800

    Updating lesson05

--- lesson05/main.rs
+++ lesson05/main.rs
@@ -1,14 +1,14 @@
 extern crate sdl2;
 
 use sdl2::Sdl;
-use sdl2::video::{Window, WindowPos, OPENGL};
-use sdl2::render::{RenderDriverIndex, ACCELERATED, Renderer, Texture};
+use sdl2::video::Window;
+use sdl2::render::{Renderer, Texture};
 use sdl2::event::Event;
-use sdl2::surface::{Surface, SWSURFACE};
+use sdl2::surface::Surface;
 use sdl2::rect::Rect;
 
-const WIDTH:  i32 = 640;
-const HEIGHT: i32 = 480;
+const WIDTH:  u32 = 640;
+const HEIGHT: u32 = 480;
 
 // This is a bit of a strange example.  It seems that the copy function
 // will automatically stretch a texture if not provided with a clipping
@@ -22,14 +22,16 @@ const HEIGHT: i32 = 480;
 /// Break out initialization into a separate function, which
 /// returns only the Window (we don't need the sdl_context)
 fn init() -> (Sdl, Window)  {
-    let sdl = sdl2::init(sdl2::INIT_VIDEO).unwrap();
-    let win = match Window::new(&sdl, "SDL Tutorial",
-                      WindowPos::PosCentered,
-                      WindowPos::PosCentered,
-                      WIDTH, HEIGHT, OPENGL) {
-        Ok(window) => window,
-        Err(err)   => panic!("Failed to create Window!: {}", err)
-    };
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    // Create the window
+    let win = match video.window("SDL Tutorial 05", WIDTH, HEIGHT)
+        .position_centered()
+        .opengl()
+        .build() {
+            Ok(window) => window,
+            Err(err)   => panic!("Failed to create Window!: {}", err)
+        };
     (sdl, win)
 }
 
@@ -37,7 +39,7 @@ fn init() -> (Sdl, Window)  {
 /// an image, and return its optimized surface.
 fn load_image(path: &'static str) -> Surface {
     use std::path::Path;
-    match Surface::from_bmp(&Path::new(path)) {
+    match Surface::load_bmp(&Path::new(path)) {
         Ok(surface) => surface,
         Err(err)    => panic!("Could not load image: {}", err)
     }
@@ -57,29 +59,31 @@ fn main() {
     // Initialize SDL2
     let (sdl_context, window) = init();
     
-    // Get a handle to the SDL2 event pump
-    let mut event_pump = sdl_context.event_pump();
+    // Get a handle to the SDL2 event pump.  This is done here because we
+    // used to need to pass the event pump to a function called 'properties_getters
+    // on the window in order to retrieve the window's pixel format.  Thank
+    // the gods that rust-sdl doesn't require such shenanigans anymore.
+    // We still need the event pump for later, so we keep the line.
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     // Load the image.  Note that we do this after we get the event_pump,
     // because in order to optimize the surface we need the PixelFormat
     // used by the window, which requires a WindowProperties, which can
     // only be obtained by passing an event pump handle to ensure no
     // unsafe operations occur while the event pump is running.
-    let image_surface = load_image("stretch.bmp");
+    let image_surface = load_image("resources/stretch.bmp");
     // Now optimize it, using the pixel format used by the window
-    let pixel_format = window
-        .properties_getters(&event_pump)
-        .get_window_pixel_format();
+    let pixel_format = window.window_pixel_format();
+    let sf_pixel_format = image_surface.pixel_format();
     let optimized_surface = match image_surface
-        .convert_format(pixel_format) {
+        .convert(&sf_pixel_format) {
             Ok(surface) => surface,
             Err(err)    => panic!("Could not convert surface: {}", err)
         };
 
     // Now stretch the optimized surface to the dimensions we want
-    let dst_rect = Rect { x: 0, y: 0, w: WIDTH, h: HEIGHT };
-    let mut stretched_surface = match Surface::new(SWSURFACE,
-                                                   WIDTH, HEIGHT, 32, 0, 0, 0, 0) {
+    let dst_rect = Rect::new(0, 0, WIDTH, HEIGHT);
+    let mut stretched_surface = match Surface::new(WIDTH, HEIGHT, pixel_format) {
         Ok(surface) => surface,
         Err(err)      => panic!("Could not create surface: {}", err)
     };
@@ -87,9 +91,7 @@ fn main() {
     // we unwrap to trigger the panic if we can't blit.
     optimized_surface.blit_scaled(None, &mut stretched_surface, Some(dst_rect)).unwrap();
 
-    let mut renderer = match Renderer::from_window(window,
-                                                   RenderDriverIndex::Auto,
-                                                   ACCELERATED) {
+    let mut renderer = match window.renderer().build() {
         Ok(renderer) => renderer,
         Err(err)     => panic!("Could not obtain renderer: {}", err)
     };
@@ -97,8 +99,6 @@ fn main() {
     // Convert the surface to a texture
     let image_texture = surface_to_texture(&stretched_surface, &renderer);
 
-    // Get a context to the renderer.
-    let mut context = renderer.drawer();
 
     // running is 'mut' because we will want to 'flip' it to false when we're ready
     // to exit the game loop.
@@ -117,8 +117,8 @@ fn main() {
             }
         }
         // Clear and render the texture each pass through the loop
-        context.clear();
-        context.copy(&image_texture, None, None);
-        context.present();
+        renderer.clear();
+        renderer.copy(&image_texture, None, None).unwrap();
+        renderer.present();
     }
 }
