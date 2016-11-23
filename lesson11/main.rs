c92cad21ec0e1319b749ef3c3d02bf71c2c4e240
Ysgard <ysgard@gmail.com>
2016-11-22 21:30:53 -0800

    Updated lesson11

--- lesson11/main.rs
+++ lesson11/main.rs
@@ -4,25 +4,27 @@ extern crate sdl2_image;
 use std::path::Path;
 
 use sdl2::Sdl;
-use sdl2::video::{Window, WindowPos, OPENGL};
-use sdl2::render::{RenderDriverIndex, ACCELERATED, Renderer, RenderDrawer, Texture};
-use sdl2::surface::{Surface};
+use sdl2::video::Window;
+use sdl2::render::{Renderer, Texture};
+use sdl2::surface::Surface;
 use sdl2::event::Event;
 use sdl2::pixels::Color;
-use sdl2::rect::{Rect, Point};
+use sdl2::rect::Rect;
 
 use sdl2_image::{LoadSurface, INIT_PNG};
 
-const WIDTH:  i32 = 640;
-const HEIGHT: i32 = 480;
+const WIDTH:  u32 = 640;
+const HEIGHT: u32 = 480;
+
+const IMG_DOTS: &'static str = "resources/dots.png";
 
 // Create a struct that will track texture data
 struct LTexture {
     // The actual texture.
     texture: Texture,
     // Image dimensions
-    width: i32,
-    height: i32
+    width: u32,
+    height: u32
 }
 
 // Implement a few functions for the Texture struct
@@ -55,7 +57,7 @@ impl LTexture {
     // Load a texture from a file
     fn new_from_file(ren: &Renderer, path: &std::path::Path) -> LTexture {
         // Load the surface first, so we can set the color key
-        let surface = match Surface::from_file(path) {
+        let mut surface = match Surface::from_file(path) {
             Ok(surface) => surface,
             Err(err)    => panic!("Could not load surface: {}", err)
         };
@@ -74,17 +76,18 @@ impl LTexture {
 
     // Renders a texture to a given point using a provided renderer
     fn render_to(&self,
-                 context: &mut RenderDrawer,
+                 renderer: &mut Renderer,
                  x: i32,
                  y: i32,
                  clip: Option<Rect>) {
         let clip_rect = match clip {
             Some(rect) => rect,
-            None       => Rect {x: 0, y: 0, w: self.width, h: self.height }
+            None       => Rect::new(0, 0, self.width, self.height)
         };
-        context.copy(&self.texture,
+        renderer.copy(&self.texture,
                      Some(clip_rect),
-                     Some(Rect { x: x, y: y, w: clip_rect.w, h: clip_rect.h}) );            
+                      Some(Rect::new(x, y, clip_rect.width(), clip_rect.height())))
+            .unwrap();            
     }
 }
 
@@ -94,16 +97,17 @@ impl LTexture {
 /// Break out initialization into a separate function, which
 /// returns only the Window (we don't need the sdl_context)
 fn init() -> (Sdl, Window)  {
-    let sdl = sdl2::init(sdl2::INIT_VIDEO).unwrap();
-    let win = match Window::new(&sdl, "SDL Tutorial",
-                      WindowPos::PosCentered,
-                      WindowPos::PosCentered,
-                      WIDTH, HEIGHT, OPENGL) {
-        Ok(window) => window,
-        Err(err)   => panic!("Failed to create Window!: {}", err)
-    };
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let win = match video.window("SDL Tutorial 11", WIDTH, HEIGHT)
+        .position_centered()
+        .opengl()
+        .build() {
+            Ok(window) => window,
+            Err(err)   => panic!("Failed to create Window!: {}", err)
+        };
 
-    sdl2_image::init(INIT_PNG);
+    sdl2_image::init(INIT_PNG).unwrap();
     
     (sdl, win)
 }
@@ -113,14 +117,14 @@ fn init() -> (Sdl, Window)  {
 // We want to avoid the use of global variables (it's not really
 // a Rust, or functional, idiom) so we return a tuple containing
 // the data
-fn load_media(ren: &Renderer, path: &std::path::Path) ->
+fn load_media(renderer: &Renderer, path: &std::path::Path) ->
     (LTexture, [Rect; 4]) {
         // Return the teuple
-        ( LTexture::new_from_file(ren, path),
-          [ Rect { x: 0, y: 0, w: 100, h: 100 },
-            Rect { x: 100, y: 0, w: 100, h:100 },
-            Rect { x: 0, y: 100, w: 100, h:100 },
-            Rect { x: 100, y: 100, w: 100, h: 100 } ] )
+        ( LTexture::new_from_file(renderer, path),
+          [ Rect::new(0, 0, 100, 100),
+            Rect::new(100, 0, 100, 100),
+            Rect::new(0, 100, 100, 100),
+            Rect::new(100, 100, 100, 100) ] )
 }
 
 
@@ -130,25 +134,19 @@ fn main() {
     let (sdl_context, window) = init();
 
     // obtain the renderer
-    let mut renderer = match Renderer::from_window(window,
-                                                   RenderDriverIndex::Auto,
-                                                   ACCELERATED) {
+    let mut renderer = match window.renderer().build() {
         Ok(renderer) => renderer,
         Err(err)     => panic!("Could not obtain renderer: {}", err)
     };
 
     // Create the textures we are going to use.
     let (sprite_sheet, sprite_clips) =
-        load_media(&renderer, Path::new("dots.png"));
+        load_media(&renderer, Path::new(IMG_DOTS));
             
-    let mut context = renderer.drawer();
-    
-    // running is 'mut' because we will want to 'flip' it to false when we're ready
-    // to exit the game loop.
     let mut running: bool = true;
 
     // Get a handle to the SDL2 event pump
-    let mut event_pump = sdl_context.event_pump();
+    let mut event_pump = sdl_context.event_pump().unwrap();
     
     // game loop
     while running {
@@ -163,30 +161,30 @@ fn main() {
             }
         }
         // Clear and render the texture each pass through the loop
-        context.set_draw_color(Color::RGB(0xff, 0xff, 0xff));
-        context.clear();
+        renderer.set_draw_color(Color::RGB(0xff, 0xff, 0xff));
+        renderer.clear();
 
         // Render top left sprite
-        sprite_sheet.render_to(&mut context, 0, 0,
+        sprite_sheet.render_to(&mut renderer, 0, 0,
                                Some(sprite_clips[0]));
 
         // Render top right sprite
-        sprite_sheet.render_to(&mut context,
-                               WIDTH - sprite_clips[1].w, 0, 
+        sprite_sheet.render_to(&mut renderer,
+                               (WIDTH - sprite_clips[1].width()) as i32, 0, 
                                Some(sprite_clips[1]));
 
         // Render bottom left sprite
-        sprite_sheet.render_to(&mut context,
-                               0, HEIGHT - sprite_clips[2].h,
+        sprite_sheet.render_to(&mut renderer,
+                               0, (HEIGHT - sprite_clips[2].height()) as i32,
                                Some(sprite_clips[2]));
 
         // Render bottom right sprite
-        sprite_sheet.render_to(&mut context,
-                               WIDTH - sprite_clips[3].w,
-                               HEIGHT - sprite_clips[3].h,
+        sprite_sheet.render_to(&mut renderer,
+                               (WIDTH - sprite_clips[3].width()) as i32,
+                               (HEIGHT - sprite_clips[3].height()) as i32,
                                Some(sprite_clips[3]));
 
         // Update the screen
-        context.present();
+        renderer.present();
     }
 }
