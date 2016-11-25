5fa1e39b72fa61a5a774cdefe6198e5ec0323b5e
Ysgard <ysgard@gmail.com>
2016-11-24 21:29:30 -0800

    Updated lesson15

--- lesson15/main.rs
+++ lesson15/main.rs
@@ -4,26 +4,28 @@ extern crate sdl2_image;
 use std::path::Path;
 
 use sdl2::Sdl;
-use sdl2::video::{Window, WindowPos, OPENGL};
-use sdl2::render::{RenderDriverIndex, ACCELERATED, Renderer, RenderDrawer, Texture};
-use sdl2::surface::{Surface};
+use sdl2::video::Window;
+use sdl2::render::{Renderer, Texture};
+use sdl2::surface::Surface;
 use sdl2::event::Event;
-use sdl2::keycode::KeyCode;
+use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::rect::{Rect, Point};
 
 use sdl2_image::{LoadSurface, INIT_PNG};
 
-const WIDTH:  i32 = 640;
-const HEIGHT: i32 = 480;
+const WIDTH:  u32 = 640;
+const HEIGHT: u32 = 480;
+
+const IMG_ARROW: &'static str = "resources/arrow.png";
 
 // Create a struct that will track texture data
 struct LTexture {
     // The actual texture.
     texture: Texture,
     // Image dimensions
-    width: i32,
-    height: i32
+    width: u32,
+    height: u32
 }
 
 // Implement a few functions for the Texture struct
@@ -54,25 +56,21 @@ impl LTexture {
         }
     }
 
-    // create a copy of 
-
     // Load a texture from a file
-    fn new_from_file(ren: &Renderer, path: &Path) -> LTexture {
+    fn new_from_file(renderer: &Renderer, path: &Path) -> LTexture {
         // Load the surface first, so we can set the color key
-        let surface = match Surface::from_file(path) {
-            Ok(surface) => surface,
-            Err(err)    => panic!("Could not load surface: {}", err)
-        };
-
+        let mut surface = Surface::from_file(path)
+            .expect("Could not load surface from file!");
+        
         // Now set the color key on the surface
-        surface.set_color_key(true, Color::RGB(0, 0xff, 0xff)).unwrap();
+        surface.set_color_key(true, Color::RGB(0, 0xff, 0xff))
+            .expect("Could not set color_key on surface!");
 
         // Convert the surface to a texture and pass it to
         // LTexture::new to be wrapped
-        let tex = match ren.create_texture_from_surface(&surface) {
-            Ok(texture) => texture,
-            Err(err)    => panic!("Could not convert surface to texture: {}", err)
-        };
+        let tex = renderer.create_texture_from_surface(&surface)
+            .expect("Could not create texture from surface!");
+
         LTexture::new(tex)
     }
 
@@ -83,40 +81,41 @@ impl LTexture {
     // going to wrap rotation and flipping args in Option<> so we can
     // provide None when we don't care about it.
     fn render_to(&self,
-                 context: &mut RenderDrawer,
+                 renderer: &mut Renderer,
                  x: i32,
                  y: i32,
                  clip: Option<Rect>,
                  rotation: Option<f64>,
                  center: Option<Point>,
-                 flip: Option<(bool, bool)>
+                 flip_h: bool,
+                 flip_v: bool
                  ) {
         let clip_rect = match clip {
             Some(rect) => rect,
-            None       => Rect {x: 0, y: 0, w: self.width, h: self.height }
+            None       => Rect::new(0, 0, self.width, self.height),
         };
         let rot: f64 = match rotation {
             Some(rot) => rot,
             None      => 0.0
         };
-        let flip = match flip {
-            Some((flip_v, flip_h)) => (flip_v, flip_h),
-            None                 => (false, false)
-        };
         
-        context.copy_ex(&self.texture,
-                        Some(clip_rect),
-                        Some(Rect { x: x, y: y, w: clip_rect.w, h: clip_rect.h}),
-                        rot,
-                        center,
-                        flip);            
+        renderer.copy_ex(&self.texture,
+                         Some(clip_rect),
+                         Some(Rect::new(x, y,
+                                        clip_rect.width(),
+                                        clip_rect.height())),
+                         rot,
+                         center,
+                         flip_h,
+                         flip_v)
+            .expect("Could not blit texture to render target!");
     }
 
     // Modulate the LTexture using a Color - this will 'tint' the texture
     // Note that LTextures are immutable, so we have to create a new one
     // and return it - we can't mutate ourselves.
     fn set_color(&mut self, color: Color) {
-        let (r, g, b) = color.get_rgb();
+        let (r, g, b) = color.rgb();
         self.texture.set_color_mod(r, g, b);
     }
 
@@ -132,16 +131,19 @@ impl LTexture {
 /// Break out initialization into a separate function, which
 /// returns only the Window (we don't need the sdl_context)
 fn init() -> (Sdl, Window)  {
-    let sdl = sdl2::init(sdl2::INIT_VIDEO).unwrap();
-    let win = match Window::new(&sdl, "SDL Tutorial",
-                      WindowPos::PosCentered,
-                      WindowPos::PosCentered,
-                      WIDTH, HEIGHT, OPENGL) {
-        Ok(window) => window,
-        Err(err)   => panic!("Failed to create Window!: {}", err)
-    };
+    let sdl = sdl2::init()
+        .expect("Could not initialize SDL!");
+    let video = sdl.video()
+        .expect("Could not obtain video from sdl context!");
+    let win = video.window("SDL Tutorial 15", WIDTH, HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .expect("Could not create Window!");
+   
 
-    sdl2_image::init(INIT_PNG);
+    sdl2_image::init(INIT_PNG)
+        .expect("Could not initialize sdl2_image!");
     
     (sdl, win)
 }
@@ -152,24 +154,17 @@ fn main() {
     let (sdl_context, window) = init();
 
     // obtain the renderer
-    let mut renderer = match Renderer::from_window(window,
-                                                   RenderDriverIndex::Auto,
-                                                   ACCELERATED) {
-        Ok(renderer) => renderer,
-        Err(err)     => panic!("Could not obtain renderer: {}", err)
-    };
+    let mut renderer = window.renderer().build()
+        .expect("Could not obtain renderer from window!");
 
-    let arrow = LTexture::new_from_file(&renderer, Path::new("arrow.png"));
+    let arrow = LTexture::new_from_file(&renderer, Path::new(IMG_ARROW));
             
-    let mut context = renderer.drawer();
-    
-    // running is 'mut' because we will want to 'flip' it to false when
-    // we're ready to exit the game loop.
     let mut running: bool = true;
 
     // Get a handle to the SDL2 event pump
-    let mut event_pump = sdl_context.event_pump();
-
+    let mut event_pump = sdl_context.event_pump()
+        .expect("Could not obtain event pump!");
+    
     // Track current rotation and flips
     let mut degrees: f64 = 0.0;
     let mut flip_vertical: bool = false;
@@ -184,41 +179,47 @@ fn main() {
                 Event::Quit {..} => {
                     running = false
                 },
-                // A and D rotate the texture by 60 degrees
-                // Q flips horizontally, E vertically, and W resets flip
-                Event::KeyDown { keycode: KeyCode::A, .. } => {
-                    degrees -= 60.0;
+                Event::KeyDown { keycode: k, .. } => match k {
+                    Some(Keycode::A) => {
+                        degrees -= 60.0;
+                    },
+                    Some(Keycode::D) => {
+                        degrees += 60.0;
+                    },
+                    Some(Keycode::Q) => {
+                        flip_horizontal = !flip_horizontal;
+                    },
+                    Some(Keycode::W) => {
+                        flip_horizontal = false;
+                        flip_vertical = false;
+                    },
+                    Some(Keycode::E) => {
+                        flip_vertical = !flip_vertical;
+                    },
+                    Some(Keycode::Escape) => {
+                        running = false;
+                    },
+                    Some(_) => {},
+                    None => {},
                 },
-                Event::KeyDown { keycode: KeyCode::D, .. } => {
-                    degrees += 60.0;
-                },
-                Event::KeyDown { keycode: KeyCode::Q, .. } => {
-                    flip_horizontal = !flip_horizontal;
-                },
-                Event::KeyDown { keycode: KeyCode::W, .. } => {
-                    flip_horizontal = false;
-                    flip_vertical = false;
-                },
-                Event::KeyDown { keycode: KeyCode::E, .. } => {
-                    flip_vertical = !flip_vertical;
-                },
-                _ => {}
+                _ => {},
             }
         }
         // Clear and render the texture each pass through the loop
-        context.set_draw_color(Color::RGB(0xff, 0xff, 0xff));
-        context.clear();
+        renderer.set_draw_color(Color::RGB(0xff, 0xff, 0xff));
+        renderer.clear();
 
         // Render the arrow
-        arrow.render_to(&mut context,
-                        (WIDTH - arrow.width) / 2,
-                        (HEIGHT - arrow.height) / 2,
+        arrow.render_to(&mut renderer,
+                        (WIDTH - arrow.width) as i32 / 2,
+                        (HEIGHT - arrow.height) as i32 / 2,
                         None,
                         Some(degrees),
                         None,
-                        Some((flip_vertical, flip_horizontal)));
+                        flip_horizontal,
+                        flip_vertical);
         
         // Update the screen
-        context.present();
+        renderer.present();
     }
 }
